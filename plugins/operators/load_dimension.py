4a9b27ac2cbb08dd2a9ff8d3506481c0bed37783
Christian Schoeppler <christian.schoeppler@mediaman.de>
2021-12-07 16:02:33 +0100

    adds functionality to operator to insert staged data to dimension tables

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -5,18 +5,42 @@ from airflow.utils.decorators import apply_defaults
 class LoadDimensionOperator(BaseOperator):
 
     ui_color = '#80BD9E'
+    
+    insert_sql = 'INSERT INTO {} {}'
+    truncate_sql = 'TRUNCATE TABLE {}'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id,
+                 table_name,
+                 sql,
+                 truncate=True,
                  *args, **kwargs):
 
+        """
+        Initializes the LoadDimensionOperator
+
+        Parameters
+        ----------
+        redshift_conn_id: id (in airflow) of the redshift connection
+        table_name: name of the table to insert data
+        sql: sql query select for data
+        truncate: optional truncate bool, when true the data inside the given table will be truncated
+        """
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id=redshift_conn_id
+        self.table_name=table_name
+        self.sql=sql
+        self.truncate=truncate
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        self.log.info('LoadDimensionOperator: Start process')
+        redshift = PostgresHook(self.redshift_conn_id)
+        
+        if (self.truncate):
+            self.log.info('Redshift: Truncate all data from {self.table_name}')
+            redshift.run(LoadDimensionOperator.truncate_sql.format(self.table_name))
+
+        self.log.info('Redshift: Insert data into dimension table {self.table_name}')
+        redshift.run(LoadDimensionOperator.insert_sql.format(self.table_name, self.sql))
+
