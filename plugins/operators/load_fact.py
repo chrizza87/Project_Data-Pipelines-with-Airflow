701547f402be570903cd3b2168754034d2a44a57
Christian Schoeppler <christian.schoeppler@mediaman.de>
2021-12-07 16:01:30 +0100

    adds functionality to operator to insert staged data to fact tables

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -5,18 +5,41 @@ from airflow.utils.decorators import apply_defaults
 class LoadFactOperator(BaseOperator):
 
     ui_color = '#F98866'
+    
+    insert_sql = 'INSERT INTO {} {}'
+    truncate_sql = 'TRUNCATE TABLE {}'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id,
+                 table_name,
+                 sql,
+                 truncate=True,
                  *args, **kwargs):
 
+        """
+        Initializes the LoadFactOperator
+
+        Parameters
+        ----------
+        redshift_conn_id: id (in airflow) of the redshift connection
+        table_name: name of the table to insert data
+        sql: sql query select for data
+        truncate: optional truncate bool, when true the data inside the given table will be truncated
+        """
         super(LoadFactOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id=redshift_conn_id
+        self.table_name=table_name
+        self.sql=sql
+        self.truncate=truncate
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        self.log.info('LoadFactOperator: Start process')
+        redshift = PostgresHook(self.redshift_conn_id)
+        
+        if (self.truncate):
+            self.log.info('Redshift: Truncate all data from {self.table_name}')
+            redshift.run(LoadFactOperator.truncate_sql.format(self.table_name))
+        
+        self.log.info('Redshift: Insert data into fact table {self.table_name}')
+        redshift.run(LoadFactOperator.insert_sql.format(self.table_name, self.sql))
